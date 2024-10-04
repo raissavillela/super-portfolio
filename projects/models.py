c2e4a4e13b7edf6cf4742ed8192fc2bbb478875c
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-10-04 12:53:45 -0700

    Requisito 3

--- projects/models.py
+++ projects/models.py
@@ -9,3 +9,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    github_url = models.URLField(max_length=500)
+    keyword = models.CharField(max_length=50)
+    key_skill = models.CharField(max_length=50)
+    profile = models.ForeignKey(
+        Profile, related_name='projects', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
-from .models import Profile
-from .serializers import ProfileSerializer
+from .models import Profile, Project
+from .serializers import ProfileSerializer, ProjectSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated]
