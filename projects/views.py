9600736ea40349d4186dd982835e707c741fbb05
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-10-07 11:13:10 -0700

    Fix requisitos 2 e 4

--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import (
+from rest_framework import viewsets, permissions
+from django.shortcuts import render, get_object_or_404
+from projects.models import (
     Profile,
     Project,
-    Certificate,
-    CertifyingInstitution)
-from .serializers import (
+    CertifyingInstitution,
+    Certificate)
+from projects.serializers import (
     ProfileSerializer,
     ProjectSerializer,
-    CertificateSerializer,
-    CertifyingInstitutionSerializer)
-from rest_framework.permissions import AllowAny, IsAuthenticated
+    CertifyingInstitutionSerializer,
+    CertificateSerializer)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -19,34 +18,27 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.request.method == 'GET':
-            return [AllowAny()]
-        return [IsAuthenticated()]
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
 
-
-def retrieve(self, request, *args, **kwargs):
-    if request.method == 'GET':
-        profile = self.get_object()
-        return render(
-            request,
-            'profile_detail.html',
-            {'profile': profile}
-            )
-    return super().retrieve(request, *args, **kwargs)
+    def retrieve(self, request, *args, **kwargs):
+        profile = get_object_or_404(Profile, pk=kwargs.get('pk'))
+        return render(request, 'profile_detail.html', {'profile': profile})
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CertifyingInstitutionViewSet(viewsets.ModelViewSet):
     queryset = CertifyingInstitution.objects.all()
     serializer_class = CertifyingInstitutionSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CertificateViewSet(viewsets.ModelViewSet):
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
